2f2b11a1cc2efbfe892df0d04d403dadbf50191f
baaesh <sanghwan@europa.snu.ac.kr>
2019-02-27 21:24:15 +0900

    added lstm encoder configs

--- config.py
+++ config.py
@@ -3,14 +3,20 @@ import argparse
 
 def model_config(parser):
     ### extractor
-    # word embedding
-    parser.add_argument('--word_dim', type=int, default=300)
-
     # convolutional sentence encoder
     parser.add_argument('--filter_sizes', nargs='+', type=int, default=[3, 4, 5])
     parser.add_argument('--num_feature_maps', type=int, default=100)
 
     ### abstracter
+
+    ### common
+    # word embedding
+    parser.add_argument('--word_dim', type=int, default=300)
+
+    # LSTM encoder
+    parser.add_argument('--lstm_hidden_units', type=int, default=256)
+    parser.add_argument('--lstm_num_layers', type=int, default=1)
+    parser.add_argument('--lstm_bidirection', default=False, action='store_true')
 
     return parser
 
@@ -24,9 +30,9 @@ def data_config(parser):
 
 
 def train_config(parser):
-    parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--mode', default='e',
                         help="available modes: e (extractor), a (abstracter), r (reinforcement)")
+    parser.add_argument('--batch_size', type=int, default=32)
     return parser
 
 
