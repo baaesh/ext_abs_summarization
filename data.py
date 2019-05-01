f90e2e8325d9561a7ef99709a0db198c55bb58a1
baaesh <sanghwan@europa.snu.ac.kr>
2019-05-01 18:08:46 +0900

    pretrained word2vec

--- data.py
+++ data.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 
 from data_utils.dataset import CnnDmDataset
 from data_utils.vocab import Vocab
+from data_utils.word2vec import make_embedding
 
 
 class CnnDm():
@@ -20,8 +21,10 @@ class CnnDm():
 
         train_examples = glob.glob(opt['train_path'] + '/*.json')
         valid_examples = glob.glob(opt['valid_path'] + '/*.json')
+        test_examples = glob.glob(opt['test_path'] + '/*.json')
         train_data = []
         valid_data = []
+        test_data = []
 
         print("Loading Training Data")
         for example in tqdm(train_examples):
@@ -31,9 +34,17 @@ class CnnDm():
         for example in tqdm(valid_examples):
             with open(example, 'r', encoding='utf-8') as file:
                 valid_data.append(json.load(file))
+        print("Loading Test Data")
+        for example in tqdm(test_examples):
+            with open(example, 'r', encoding='utf-8') as file:
+                test_data.append(json.load(file))
+
+        print('Loading Word2Vec pretrained vectors')
+        self.vectors, _ = make_embedding(self.vocab, 'data/word2vec/word2vec.128d.226k.bin')
 
         self.train_dataset = CnnDmDataset(opt, train_data, self.vocab, opt['mode'])
         self.valid_dataset = CnnDmDataset(opt, valid_data, self.vocab, opt['mode'])
+        self.test_dataset = CnnDmDataset(opt, test_data, self.vocab, 't')
 
         self.train_loader = DataLoader(dataset=self.train_dataset,
                                        batch_size=opt['batch_size'],
@@ -41,5 +52,9 @@ class CnnDm():
                                        collate_fn=self.train_dataset.collate)
         self.valid_loader = DataLoader(dataset=self.valid_dataset,
                                        batch_size=opt['batch_size'],
-                                       shuffle=False,
+                                       shuffle=True,
                                        collate_fn=self.valid_dataset.collate)
+        self.test_loader = DataLoader(dataset=self.test_dataset,
+                                      batch_size=opt['batch_size'],
+                                      shuffle=False,
+                                      collate_fn= self.test_dataset.collate)
